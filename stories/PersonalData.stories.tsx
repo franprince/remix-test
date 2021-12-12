b34d33519aafd06e70e72ff4c3d12bb2933b4766
root <root@DESKTOP-HE4CRE9.localdomain>
2021-12-12 19:54:24 -0300

    Add stories

---
+++ stories/PersonalData.stories.tsx
@@ -0,0 +1,24 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { PersonalData } from "../app/components";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Components/PersonalData",
+  component: PersonalData,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+} as ComponentMeta<typeof PersonalData>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof PersonalData> = (args) => (
+  <PersonalData {...args} />
+);
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  name: "Francisco Prince",
+  location: "La Plata",
+  jobTitle: "Frontend developer",
+  availability: "No disponible",
+};
